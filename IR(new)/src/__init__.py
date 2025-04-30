7a5decbc60e1b3fad3bcb2a80d042a82bd6e96ae
unknown <raimab222@gmail.com>
2025-04-30 21:10:07 +0530

    New changes

--- IR(new)/src/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes src folder a python package, so it can be imported by the index.html.
--- IR(new)/src/check.py
+++
@@ -1,8 +0,0 @@
-import os
-
-model_path = 'models/plant_disease_model.h5'
-
-if os.path.exists(model_path):
-    print("Model file found!")
-else:
-    print("Model file NOT found!")
